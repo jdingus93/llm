f55621d42841c04c18d1491846c08e52024c2d8b
jdingus93 <jdingus93@yahoo.com>
2025-09-23 17:05:37 -0500

    added test cases to make sure the calculator function works

---
+++ functions/get_files_info.py
@@ -0,0 +1,24 @@
+import os
+
+
+def get_files_info(working_directory, directory="."):
+    base_abs = os.path.abspath(working_directory)
+    target_joined = os.path.join(working_directory, directory)
+    target_abs = os.path.abspath(target_joined)
+    if not (target_abs == base_abs or target_abs.startswith(base_abs + os.sep)):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    if not os.path.isdir(target_abs):
+        return f'Error: "{directory}" is not a directory'
+    try:
+        lines = []
+        for name in os.listdir(target_abs):
+            full_path = os.path.join(target_abs, name)
+            size = os.path.getsize(full_path)
+            is_dir = os.path.isdir(full_path)
+            line = f'- {name}: file_size={size} bytes, is_dir={is_dir}'
+            lines.append(line)
+
+        return "\n".join(lines)
+    except Exception as e:
+        return f"Error: {e}"
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -14,16 +15,32 @@ def main():
     if len(sys.argv) == 1:
         print("Unable to answer empty prompt")
         sys.exit(1)
-    else:
-        prompt="".join(sys.argv[1:])
-        response = client.models.generate_content(
-            model="gemini-2.0-flash-001",
-            contents=prompt
-        )
 
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    is_verbose = (sys.argv[-1] == "--verbose")
+    if is_verbose:
+        prompt_parts = sys.argv[1:-1]
+    else:
+        prompt_parts = sys.argv[1:]
+
+    if not prompt_parts:
+        print("Unable to answer empty prompt")
+        sys.exit(1)
+
+    user_prompt="".join(prompt_parts)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages
+    )
+
+    if is_verbose:
+        print(f"User prompt: {response.text}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
